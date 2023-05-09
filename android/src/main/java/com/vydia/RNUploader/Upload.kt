ec65683b4ebf2a4e40e452bd3be7dd6753ed474d
thomasvo <thomasttvo@gmail.com>
2023-05-09 12:49:43 -0700

    simplify

--- android/src/main/java/com/vydia/RNUploader/Upload.kt
+++ android/src/main/java/com/vydia/RNUploader/Upload.kt
@@ -25,14 +25,14 @@ data class Upload(
     IllegalArgumentException("Missing '$optionName'")
 
   companion object {
-    fun fromOptions(options: ReadableMap) = Upload(
-      id = options.getString("customUploadId") ?: UUID.randomUUID().toString(),
-      url = options.getString("url") ?: throw MissingOptionException("url"),
-      path = options.getString("path") ?: throw MissingOptionException("path"),
-      method = (options.getString("method") ?: "POST").let { HttpMethod.parse(it) },
-      maxRetries = if (options.hasKey("maxRetries")) options.getInt("maxRetries") else 5,
-      wifiOnly = if (options.hasKey("wifiOnly")) options.getBoolean("wifiOnly") else false,
-      headers = options.getMap("headers").let { headers ->
+    fun fromOptions(o: ReadableMap) = Upload(
+      id = o.getString("customUploadId") ?: UUID.randomUUID().toString(),
+      url = o.getString(Upload::url.name) ?: throw MissingOptionException(Upload::url.name),
+      path = o.getString(Upload::path.name) ?: throw MissingOptionException(Upload::path.name),
+      method = (o.getString(Upload::method.name) ?: "POST").let { HttpMethod.parse(it) },
+      maxRetries = if (o.hasKey(Upload::maxRetries.name)) o.getInt(Upload::maxRetries.name) else 5,
+      wifiOnly = if (o.hasKey(Upload::wifiOnly.name)) o.getBoolean(Upload::wifiOnly.name) else false,
+      headers = o.getMap(Upload::headers.name).let { headers ->
         if (headers == null) return@let mapOf()
         val map = mutableMapOf<String, String>()
         for (entry in headers.entryIterator) {
@@ -40,16 +40,16 @@ data class Upload(
         }
         return@let map
       },
-      notificationId = options.getString("notificationId")
-        ?: throw MissingOptionException("notificationId"),
-      notificationTitle = options.getString("notificationTitle")
-        ?: throw MissingOptionException("notificationTitle"),
-      notificationTitleNoInternet = options.getString("notificationTitleNoInternet")
-        ?: throw MissingOptionException("notificationTitleNoInternet"),
-      notificationTitleNoWifi = options.getString("notificationTitleNoWifi")
-        ?: throw MissingOptionException("notificationTitleNoWifi"),
-      notificationChannel = options.getString("notificationChannel")
-        ?: throw MissingOptionException("notificationChannel"),
+      notificationId = o.getString(Upload::notificationId.name)
+        ?: throw MissingOptionException(Upload::notificationId.name),
+      notificationTitle = o.getString(Upload::notificationTitle.name)
+        ?: throw MissingOptionException(Upload::notificationTitle.name),
+      notificationTitleNoInternet = o.getString(Upload::notificationTitleNoInternet.name)
+        ?: throw MissingOptionException(Upload::notificationTitleNoInternet.name),
+      notificationTitleNoWifi = o.getString(Upload::notificationTitleNoWifi.name)
+        ?: throw MissingOptionException(Upload::notificationTitleNoWifi.name),
+      notificationChannel = o.getString(Upload::notificationChannel.name)
+        ?: throw MissingOptionException(Upload::notificationChannel.name),
     )
   }
 }
